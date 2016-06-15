27f8149ab8b2aa425cc8d08dab4fa1cf3b7b488e
Matt Allan <matthew.james.allan@gmail.com>
2016-06-15 16:46:04 -0400

    Only allow RFC3339 Dates

--- src/Constraints/Format.php
+++ src/Constraints/Format.php
@@ -8,10 +8,9 @@ use League\JsonGuard\ValidationError;
 
 class Format implements PropertyConstraint
 {
-    // regex from http://www.pelagodesign.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/
-    // @codingStandardsIgnoreStart
-    const DATE_TIME_PATTERN = '/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/';
-    // @codingStandardsIgnoreEnd
+    const DATE_TIME_PATTERN = '/^([0-9]{4})-([0-9]{2})-([0-9]{2})'
+    . '([Tt]([0-9]{2}):([0-9]{2}):([0-9]{2})(\\.[0-9]+)?)?'
+    . '(([Zz]|([+-])([0-9]{2}):([0-9]{2})))?/';
 
     const HOST_NAME_PATTERN = '/^[_a-z]+\.([_a-z]+\.?)+$/i';
 
